4da40b5c5cfa9c6e773ba4bf654fae81294c6525
TommyDong1998 <tommydong1998@gmail.com>
2022-07-11 14:13:30 -0700

    shorten example

--- example/index.js
+++ example/index.js
@@ -27,13 +27,6 @@ monster.setVelocity(10,0);	//Move right at 10 a second
 const grass=map.e(Monster);
 
 grass.setPosition(150,100);
-var prev={};
-monster.on("NextFrame",()=>{
-	if(prev.x!=monster.left().x||prev.y!=monster.left().y){
-		console.log("Monster:",monster.left())
-		prev=monster.left()
-	}
-})
 
 monster.on("collide",(collision)=>{
 	console.log("Walking through grass")
